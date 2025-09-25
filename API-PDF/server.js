6b1d0c1272e861fc553e3dcfb1ac3dd9afd51d49
José Robério <jose.silva2399@etec.sp.gov.br>
2025-09-24 21:25:49 -0300

    Update server.js

--- API-PDF/server.js
+++ API-PDF/server.js
@@ -1,33 +1,38 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
 const pdfParse = require('pdf-parse');
-const fs = require('fs')
-const porta = 3000;
+const fs = require('fs');
 
 const app = express();
-const upload = multer ({ dest: 'uploads/'})
+const porta = process.env.PORT || 3000;
 
+// Configuração CORS
+app.use(cors());
+
+// Configuração Multer
+const upload = multer({ dest: 'uploads/' });
+
+// Rota para ler PDF
 app.post('/ler', upload.single('arquivo'), async (req, res) => {
-    try {
-        const arquivo = req.file;
+  try {
+    const arquivo = req.file;
+    if (!arquivo) {
+      return res.status(400).json({ erro: 'Arquivo PDF necessário!' });
+    }
 
-        if (!arquivo) {
-            return res.status(400).json({erro: 'Arquivo PDF necessário!'});
-        }
-    
     const dataBuffer = fs.readFileSync(arquivo.path);
-
     const data = await pdfParse(dataBuffer);
 
-    fs.unlinkSync(arquivo.path);
+    fs.unlinkSync(arquivo.path); // remove o arquivo temporário
 
-    res.json ({ texto: data.text.trim() });
-    } catch (error) {
-        res.status(500).json({erro: 'Erro ao ler o PDF.', detalhes: error.message });
-    }
+    res.json({ texto: data.text.trim() });
+  } catch (error) {
+    res.status(500).json({ erro: 'Erro ao ler o PDF.', detalhes: error.message });
+  }
 });
 
 app.listen(porta, () => {
-    console.log (`API rodando em http://localhost:${porta}`);
+  console.log(`API rodando em http://localhost:${porta}`);
 });
